0c9a36b522e99f2c7ba0988a51244914f24b99d6
Jinanshi Shah <102961374+Jinanshii09@users.noreply.github.com>
2024-08-31 23:04:02 +0530

    Added constructor and basic fields to Book class

--- src/main/java/com/example/library/Book.java
+++ src/main/java/com/example/library/Book.java
@@ -1,5 +1,26 @@
 package com.example.library;
 
 public class Book {
-    
+    private String title;
+    private String author;
+    private boolean isBorrowed;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isBorrowed = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
 }
