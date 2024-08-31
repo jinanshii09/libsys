cffde1dd30bcc6f5d50276d30da4a7323fd4de8b
Jinanshi Shah <102961374+Jinanshii09@users.noreply.github.com>
2024-08-31 23:05:44 +0530

    Added borrow() and returnBook() method in Book class

--- src/main/java/com/example/library/Book.java
+++ src/main/java/com/example/library/Book.java
@@ -23,4 +23,26 @@ public class Book {
         return isBorrowed;
     }
 
+    public void borrow() {
+        if (isBorrowed) {
+            System.out.println("This book is already borrowed.");
+        } else {
+            isBorrowed = true;
+            System.out.println("You have borrowed the book: " + title);
+        }
+    }
+
+    public void returnBook() {
+        if (isBorrowed) {
+            isBorrowed = false;
+            System.out.println("You have returned the book: " + title);
+        } else {
+            System.out.println("This book wasn't borrowed.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return title + " by " + author + (isBorrowed ? " (Borrowed)" : " (Available)");
+    }
 }
